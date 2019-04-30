c1d91bc632c52ba5364f1c06f31c39996bafaf74
Daniel Monetelli <danielmonetelli@hotmail.com>
2019-04-29 22:53:48 -0500

    Start XF_PlanetXamarin

---
+++ XF_PlanetXamarin/Views/BlogPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XF_PlanetXamarin.Views
+{
+    public partial class BlogPage : ContentPage
+    {
+        public BlogPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
